ff84dafe387b6fa709069d698c3103237dd8583a
张宇 <3095848613@qq.com>
2022-09-16 15:51:12 +0800

    爬取的python

---
+++ crawinformation.py
@@ -0,0 +1,53 @@
+import re
+
+import openpyxl as op
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+
+def write_to_excel(data):
+    wb = op.Workbook()
+    ws = wb['Sheet']
+    ws.append(['序号', '标题', '日期', '阅读量'])
+    dfData = {
+        '序号': data[0],
+        '标题': data[1],
+        '日期': data[2],
+        '阅读量': data[3]
+    }
+    # 创建DataFrame
+    df = pd.DataFrame(dfData)
+    # 存表，去除原始索引列（0,1,2...）
+    df.to_excel('result.xlsx', index=False)
+
+
+def craw():
+    n = 50
+    title, date, read = list(), list(), list()
+    for page in range(1, n + 1):
+        r = requests.get("https://www.aquanliang.com/blog/page/%d" % page)
+        content = r.text
+        soup = BeautifulSoup(content, 'html.parser')
+        rows = soup.find_all(attrs={'class': '_3HG1uUQ3C2HBEsGwDWY-zw'})
+
+        for row in rows:
+            # 标题
+            _title = row.find(attrs={'class': '_3_JaaUmGUCjKZIdiLhqtfr'})
+            title.append(_title.get_text())
+
+            _date = row.find(attrs={'class': '_3TzAhzBA-XQQruZs-bwWjE'})
+            d = str(_date)
+            res = re.search(r"(\d{4}-\d{1,2}-\d{1,2})", d)
+            date.append(res.group())
+
+            # 阅读量
+            _read = row.find(attrs={'class': '_2gvAnxa4Xc7IT14d5w8MI1'})
+            read.append(_read.get_text())
+    nums = [i for i in range(1, len(title) + 1)]
+    data = [nums, title, date, read]
+    write_to_excel(data)
+
+
+if __name__ == '__main__':
+    craw()
