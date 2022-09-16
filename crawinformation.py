c0046834af44b0e00fab2d5d4b53cb7d7e344cb2
张宇 <3095848613@qq.com>
2022-09-16 16:00:19 +0800

    增加了代码的注释

--- crawinformation.py
+++ crawinformation.py
@@ -23,29 +23,40 @@ def write_to_excel(data):
 
 
 def craw():
+    # n为爬取的总页数
     n = 50
+    # title存储标题, date存储日期, read存储阅读量
     title, date, read = list(), list(), list()
     for page in range(1, n + 1):
+        # get函数发送get请求
         r = requests.get("https://www.aquanliang.com/blog/page/%d" % page)
+        # r.text表示服务端返回的内容
         content = r.text
+        # 使用BeautifulSoup解析html代码
         soup = BeautifulSoup(content, 'html.parser')
+        # 使用使用BeautifulSoup解析html代码获取class属性等于_3HG1uUQ3C2HBEsGwDWY-zw的标签
         rows = soup.find_all(attrs={'class': '_3HG1uUQ3C2HBEsGwDWY-zw'})
-
+        # 遍历当前这一页的内容
         for row in rows:
             # 标题
             _title = row.find(attrs={'class': '_3_JaaUmGUCjKZIdiLhqtfr'})
             title.append(_title.get_text())
 
+            # 日期
             _date = row.find(attrs={'class': '_3TzAhzBA-XQQruZs-bwWjE'})
             d = str(_date)
+            # 使用search方法匹配日期
             res = re.search(r"(\d{4}-\d{1,2}-\d{1,2})", d)
+            # group函数获取日期
             date.append(res.group())
 
             # 阅读量
             _read = row.find(attrs={'class': '_2gvAnxa4Xc7IT14d5w8MI1'})
             read.append(_read.get_text())
+    # nums表示序号
     nums = [i for i in range(1, len(title) + 1)]
     data = [nums, title, date, read]
+    # 写入到excel中
     write_to_excel(data)
 
 
